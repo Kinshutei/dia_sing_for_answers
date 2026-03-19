2af4f2865d9c4817a9ea45127653e30b1c35f305
Kinshutei <whitelily.goldeneagle.inn@gmail.com>
2026-03-19 17:07:36 +0900

    Update types.ts

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,10 +1,21 @@
+export interface SongMaster {
+  song_id: string
+  楽曲名: string
+  原曲アーティスト: string
+  作詞: string
+  作曲: string
+  リリース日: string
+}
+
 export interface StreamingRecord {
   枠名: string
+  song_id: string
   楽曲名: string
   歌唱順: number
   配信日: string
   枠URL: string
   コラボ相手様: string
+  // マスターから JOIN されるフィールド
   原曲Artist: string
   作詞: string
   作曲: string
